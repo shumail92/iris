94f8259d41ee36977c61e50e5d96719d0332d7c2
Shumail92 <shumailmohyuddin@gmail.com>
2015-07-30 14:21:33 +0200

    [lpm]: Comm. with TLC5947. Setting pin & PWM or reset all

--- tools/lpm.cc
+++ tools/lpm.cc
@@ -7,7 +7,6 @@
 int main(int argc, char **argv) {
 
     std::string device;
-    char in2put;
     std::string input2;
 
     namespace po = boost::program_options;
@@ -17,10 +16,6 @@ int main(int argc, char **argv) {
             ("help",    "Some help stuff")
             ("device", po::value<std::string>(&device), "device file for aurdrino")
             ("input", po::value<std::string>(&input2), "send r/g/b");
-
-    std::stringstream ss;
-    ss << input2 << "\n";
-    input2 = ss.str();
 
     std::cout << "setting input2:" << input2 << std::endl;
 
@@ -33,21 +28,24 @@ int main(int argc, char **argv) {
     } else if (vm.count("device")) {
         std::cout << "Opening device: " << vm["device"].as<std::string>() << std::endl;
 
-        device::serial a = device::arduino::open(device);
+        device::serial io = device::arduino::open(device);
 
-        std::cout << "val of fd: " << a.printfd() << std::endl;
+        std::cout << "val of fd: " << io.printfd() << std::endl;
 
         std:: cout << "input: " << input2 << std::endl;
 
-        a.send_data(input2);
+        if(input2 == "reset") {
+            input2 = "-1,-1";
+            std::cout << "sending: " << input2 << std::endl;
+        }
+
+        io.send_data(input2);
         //write(a.printfd(),  &input,  1);
 
-        char output;
-        int index = 0;
         std::cout  << "from arduino: ";
 
         while(1) {
-            std::string data = a.recv_line(1000);
+            std::string data = io.recv_line(1000);
             std::cout << data << std::endl;
 
             if(data == std::string("*")) {
